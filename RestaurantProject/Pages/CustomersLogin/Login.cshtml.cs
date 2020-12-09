afb6a2c83718d32e5604ca56a30213b671cc1979
rui <sunruigooduk@gmail.com>
2020-12-09 17:04:26 +0000

    Add project files.

---
+++ RestaurantProject/Pages/CustomersLogin/Login.cshtml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using RestaurantProject.Models;
+
+namespace RestaurantProject.Pages.CustomersLogin
+{
+    f
+}
